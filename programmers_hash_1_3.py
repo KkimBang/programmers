3458fbc89e4df420259f6f6b34a5568f717024dc
bh_Kim <60415103+KkimBang@users.noreply.github.com>
2022-02-22 16:14:08 +0900

    Update programmers_hash_1_3.py

--- programmers_hash_1_3.py
+++ programmers_hash_1_3.py
@@ -5,11 +5,8 @@ from functools import reduce
 def solution(clothes):
     answer = 0
     d = {}
-    print("!! : ", [y for x, y in clothes])
     cnt = Counter([kind for name, kind in clothes])
-    print("cnt : ", cnt.values())
     result = reduce(lambda x, y: x * (y+1), cnt.values(), 1) - 1
-    print("result : ", result)
     # for item in clothes:
     #     if item[1] not in d:
     #         d[item[1]] = [item[0]]
